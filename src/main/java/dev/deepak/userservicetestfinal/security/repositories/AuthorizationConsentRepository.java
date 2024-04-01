60d0ed21ca303565635b38e607195a43b3758acf
shabiulhassan <40273632+shabiulhassan@users.noreply.github.com>
2024-04-01 07:18:37 +0530

    registering user and authorize by making models and repositories

---
+++ src/main/java/dev/deepak/userservicetestfinal/security/repositories/AuthorizationConsentRepository.java
@@ -0,0 +1,14 @@
+package dev.deepak.userservicetestfinal.security.repositories;
+
+import java.util.Optional;
+
+
+import dev.deepak.userservicetestfinal.security.models.AuthorizationConsent;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorizationConsentRepository extends JpaRepository<AuthorizationConsent, AuthorizationConsent.AuthorizationConsentId> {
+    Optional<AuthorizationConsent> findByRegisteredClientIdAndPrincipalName(String registeredClientId, String principalName);
+    void deleteByRegisteredClientIdAndPrincipalName(String registeredClientId, String principalName);
+}
